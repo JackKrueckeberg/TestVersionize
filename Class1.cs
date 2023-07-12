4fccdbdce5a0d1667a71084879365adc20ff1d00
Jack Krueckeberg <jkrueckeberg@bastiansolutions.com>
2023-07-12 10:42:54 -0400

    fix: added a print

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,12 @@
-﻿namespace TestVersionize;
+﻿using System;
+
+namespace TestVersionize;
 
 public class Class1
 {
     public static void Main() {
         var hello = "hello world";
+
+        Console.WriteLine(hello);
     }
 }
